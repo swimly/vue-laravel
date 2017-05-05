e3235d7893adafe2c6f3a4d145beaff553fbe263
unknown <>
2017-05-05 17:57:54 +0800

    add material

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,6 +2,11 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view></router-view>
+    <md-bottom-bar>
+      <md-bottom-bar-item md-iconset="icon-speedometer">Recents</md-bottom-bar-item>
+      <md-bottom-bar-item md-iconset="icon-heart" md-active>Favorites</md-bottom-bar-item>
+      <md-bottom-bar-item md-iconset="icon-cursor">Nearby</md-bottom-bar-item>
+    </md-bottom-bar>
   </div>
 </template>
 
@@ -12,12 +17,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
